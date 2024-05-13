3f15b6ac255259abfe0522819c84497e86eedad2
Jerina-Wangkhem <161306085+Jerina-Wangkhem@users.noreply.github.com>
2024-05-13 08:59:41 +0530

    Add files via upload

---
+++ Hospital.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Hospital.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Appoinment> Appointments { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Contact> contacts { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<HospitalInfo> HospitalInfos { get; set; }
+        public DbSet<Insurance> Insurances { get; set; }
+        public DbSet<Lab> Labs { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<MedicineReport> MedicinesReports { get; set; }
+        public DbSet<Payroll> Payrolls { get; set; }
+        public DbSet<PescribedMedicine> pescribedMedicines { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<TestPrice> TestPrices { get; set; }
+    }
+
+}
---
+++ Hospital.Repositories/Interface/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories.Interface
+{
+    public interface IGenericRepository<T> : IDisposable
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> ordereBy = null, string includeproperties = "");
+
+        T GetById(Object id);
+        Task<T> GetByIdAsync(Object id);
+        void Add(T entity);
+        Task<T> AddAsync(T entity);
+        void Update(T entity);
+        Task<T> UpdateAsync(T entity);
+        void Delete(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+}
