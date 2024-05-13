c6a3463987f1989594a00c9255af03632e1145c3
Jerina-Wangkhem <161306085+Jerina-Wangkhem@users.noreply.github.com>
2024-05-13 09:08:05 +0530

    Add files via upload

---
+++ Hospital.Utilities/WebsiteRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Utilities
+{
+    public class WebsiteRoles
+    {
+        public const String Website_Admin = "Admin";
+        public const String Website_Patient = "Patient";
+        public const String Website_Doctor = "Doctor";
+    }
+}
